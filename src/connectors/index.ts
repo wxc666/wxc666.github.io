b0ebb502d51acb31c35c784bb1afa24042498f1a
wxc <wxc_002@163.com>
2023-05-26 18:32:34 +0800

    update

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -32,7 +32,7 @@ export const injected = new InjectedConnector({
 
 // mainnet only
 export const walletconnect = new WalletConnectConnector({
-  rpc: { 1: NETWORK_URL },
+  rpc: { [NETWORK_CHAIN_ID]: NETWORK_URL },
   bridge: 'https://bridge.walletconnect.org',
   qrcode: true,
   pollingInterval: 15000
